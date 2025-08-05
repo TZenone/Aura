b6a89dd8c135344d6934a97efb206dc180b3c8f2
TZenone <tristan.zenone@gmail.com>
2025-08-05 12:08:04 +0200

    Added Player and Enemy classes

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,17 @@
+// Copyright Tristan ZENONE
+
+
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
