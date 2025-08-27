3bbd4c38f1ba6124c74ae4810822e681b4d19af9
TZenone <tristan.zenone@gmail.com>
2025-08-27 15:30:07 +0200

    Added custom MMCs for MaxHealth and MaxMana b ased on Vigor and int respectively

---
+++ Source/Aura/Private/AbilitySystem/ModMagCalc/MMC_MaxHealth.cpp
@@ -0,0 +1,39 @@
+// Copyright Tristan ZENONE
+
+
+#include "AbilitySystem/ModMagCalc/MMC_MaxHealth.h"
+
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "Interaction/CombatInterface.h"
+
+UMMC_MaxHealth::UMMC_MaxHealth()
+{
+	VigorDef.AttributeToCapture = UAuraAttributeSet::GetVigorAttribute();
+	VigorDef.AttributeSource = EGameplayEffectAttributeCaptureSource::Target;
+	VigorDef.bSnapshot = false;
+
+	RelevantAttributesToCapture.Add(VigorDef);
+}
+
+float UMMC_MaxHealth::CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const
+{
+	// Gather Tags from source and target
+	const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+
+	FAggregatorEvaluateParameters EvaluationParams;
+	EvaluationParams.SourceTags = SourceTags;
+	EvaluationParams.TargetTags = TargetTags;
+
+	// Get the vigor
+	float Vigor = 0.f;
+	GetCapturedAttributeMagnitude(VigorDef, Spec, EvaluationParams, Vigor);
+	Vigor = FMath::Max<float>(Vigor, 0.f);
+
+	// Get the Player level from Combat interface
+	ICombatInterface* CombatInterface = Cast<ICombatInterface>(Spec.GetContext().GetSourceObject());
+	const int32 PlayerLevel = CombatInterface->GetPlayerLevel();
+
+	// Custom mmc formula for max health, based on both Vigor and Player Level
+	return 90.f + 2.5f * Vigor + 10.f * PlayerLevel;
+}
---
+++ Source/Aura/Private/AbilitySystem/ModMagCalc/MMC_MaxMana.cpp
@@ -0,0 +1,39 @@
+// Copyright Tristan ZENONE
+
+
+#include "AbilitySystem/ModMagCalc/MMC_MaxMana.h"
+
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "Interaction/CombatInterface.h"
+
+UMMC_MaxMana::UMMC_MaxMana()
+{
+	IntelligenceDef.AttributeToCapture = UAuraAttributeSet::GetIntelligenceAttribute();
+	IntelligenceDef.AttributeSource = EGameplayEffectAttributeCaptureSource::Target;
+	IntelligenceDef.bSnapshot = false;
+
+	RelevantAttributesToCapture.Add(IntelligenceDef);
+}
+
+float UMMC_MaxMana::CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const
+{
+	// Gather Tags from source and target
+	const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+
+	FAggregatorEvaluateParameters EvaluationParams;
+	EvaluationParams.SourceTags = SourceTags;
+	EvaluationParams.TargetTags = TargetTags;
+
+	// Get the Intelligence from 
+	float Intelligence = 0.f;
+	GetCapturedAttributeMagnitude(IntelligenceDef, Spec, EvaluationParams, Intelligence);
+	Intelligence = FMath::Max<float>(Intelligence, 0.f);
+
+	// Get the Player level from Combat interface
+	ICombatInterface* CombatInterface = Cast<ICombatInterface>(Spec.GetContext().GetSourceObject());
+	const int32 PlayerLevel = CombatInterface->GetPlayerLevel();
+
+	// Custom mmc formula for max health, based on both Intelligence and Player Level
+	return 50.f + 2.5f * Intelligence + 15.f * PlayerLevel;
+}
