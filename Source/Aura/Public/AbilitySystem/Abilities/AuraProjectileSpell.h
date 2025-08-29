514545d4dab174b993b100f972aaa3ad869b71b3
TZenone <tristan.zenone@gmail.com>
2025-08-29 15:22:21 +0200

    Added Aura projectile base Actor class and AuraProjectileSpell Ability base class

---
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraProjectileSpell.h
@@ -0,0 +1,17 @@
+// Copyright Tristan ZENONE
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/AuraGameplayAbility.h"
+#include "AuraProjectileSpell.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraProjectileSpell : public UAuraGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
