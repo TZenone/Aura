5e8378fbdb3406ebe51788ea1181376087ea4296
TZenone <tristan.zenone@gmail.com>
2025-09-24 16:27:24 +0200

    Added Data Asset and correspoding data tables for enemy Character classes, and plugged everything together

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+// Copyright Tristan ZENONE
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
