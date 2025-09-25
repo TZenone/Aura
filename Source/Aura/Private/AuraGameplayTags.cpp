05964a5c0c5e5b9458ae1e46c6f3c4505921fea1
TZenone <tristan.zenone@gmail.com>
2025-09-25 14:45:00 +0200

    Added HitReact to ebase charcter class using gameplay tags

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -77,6 +77,9 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
 	GameplayTags.Damage = UGameplayTagsManager::Get().AddNativeGameplayTag(
 		FName("Damage"),
 		FString("Damage"));
+	GameplayTags.Effects_HitReact = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Effects.HitReact"),
+		FString("Tag granted when Hit reacting"));
 	
 	// Vital Attributes
 	/*GameplayTags.Attributes_Vital_Health = UGameplayTagsManager::Get().AddNativeGameplayTag(
--- Source/Aura/Public/AuraGameplayTags.h
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -44,7 +44,7 @@ public:
 	FGameplayTag InputTag_4;
 
 	FGameplayTag Damage;
-	
+	FGameplayTag Effects_HitReact;
 	// Vital Attributes
 	/*FGameplayTag Attributes_Vital_Health;
 	FGameplayTag Attributes_Vital_Mana;*/
