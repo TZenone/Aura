14347b9c9ee95d62e395b1e7a00a82355e2da310
TZenone <tristan.zenone@gmail.com>
2025-10-03 15:10:46 +0200

    Fixing some multiplayer stuff on floating text and added gameplay tags for weapon sockets

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -121,4 +121,15 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
 	GameplayTags.Abilities_Attack = UGameplayTagsManager::Get().AddNativeGameplayTag(
 		FName("Abilities.Attack"),
 		FString("Attack ability tag"));
+
+	// Montage
+	GameplayTags.Montage_Attack_Weapon = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Montage.Attack.Weapon"),
+		FString("Weapon"));
+	GameplayTags.Montage_Attack_RightHand = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Montage.Attack.RightHand"),
+		FString("Right Hand"));
+	GameplayTags.Montage_Attack_LeftHand = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Montage.Attack.LeftHand"),
+		FString("Left Hand"));
 }
--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -127,7 +127,10 @@ void AAuraEnemy::HitReactTagChanged(const FGameplayTag CallbackTag, int32 NewCou
 {
 	bHitReacting = NewCount > 0;
 	GetCharacterMovement()->MaxWalkSpeed = bHitReacting ? 0.f : BaseWalkSpeed;
-	AuraAIController->GetBlackboardComponent()->SetValueAsBool(FName("HitReacting"), true);
+	if (AuraAIController && AuraAIController->GetBlackboardComponent())
+	{
+		AuraAIController->GetBlackboardComponent()->SetValueAsBool(FName("HitReacting"), true);
+	}
 }
 
 void AAuraEnemy::InitAbilityActorInfo()
