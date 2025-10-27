804afb1662973620be083c47ddb2f0760933f6ea
TZenone <tristan.zenone@gmail.com>
2025-10-27 16:52:58 +0100

    Ability cooldown on the widget, this was hard

--- Source/Aura/Private/AbilitySystem/AsyncTasks/WaitCooldownChange.cpp
+++ Source/Aura/Private/AbilitySystem/AsyncTasks/WaitCooldownChange.cpp
@@ -9,21 +9,21 @@ UWaitCooldownChange* UWaitCooldownChange::WaitForCooldownChange(UAbilitySystemCo
 	UWaitCooldownChange* WaitCooldownChange = NewObject<UWaitCooldownChange>();
 	WaitCooldownChange->ASC = AbilitySystemComponent;
 	WaitCooldownChange->CooldownTag = InCooldownTag;
-
+	
 	if (!IsValid(AbilitySystemComponent) || !InCooldownTag.IsValid())
 	{
 		WaitCooldownChange->EndTask();
 		return nullptr;
 	}
 
-	// To know when a cooldown has ended
+	// To know when a cooldown has ended (Cooldown Tag has been removed)
 	AbilitySystemComponent->RegisterGameplayTagEvent(
 		InCooldownTag,
 		EGameplayTagEventType::NewOrRemoved).AddUObject(
 			WaitCooldownChange,
 			&UWaitCooldownChange::CooldownTagChanged);
 
-	// To know when a cooldown effect has been applied.
+	// To know when a cooldown effect has been applied
 	AbilitySystemComponent->OnActiveGameplayEffectAddedDelegateToSelf.AddUObject(WaitCooldownChange, &UWaitCooldownChange::OnActiveEffectAdded);
 
 	return WaitCooldownChange;
@@ -59,19 +59,18 @@ void UWaitCooldownChange::OnActiveEffectAdded(UAbilitySystemComponent* TargetASC
 	{
 		FGameplayEffectQuery GameplayEffectQuery = FGameplayEffectQuery::MakeQuery_MatchAnyOwningTags(CooldownTag.GetSingleTagContainer());
 		TArray<float> TimesRemaining = ASC->GetActiveEffectsTimeRemaining(GameplayEffectQuery);
-
 		if (TimesRemaining.Num() > 0)
 		{
-			int32 Index = 0;
 			float TimeRemaining = TimesRemaining[0];
-			for (int32 i = 1; i < TimesRemaining.Num(); i++)
+			for (int32 i = 0; i < TimesRemaining.Num(); i++)
 			{
 				if (TimesRemaining[i] > TimeRemaining)
 				{
 					TimeRemaining = TimesRemaining[i];
 				}
 			}
-			CooldownEnd.Broadcast(TimeRemaining);
+			
+			CooldownStart.Broadcast(TimeRemaining);
 		}
 	}
 }
